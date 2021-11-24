008d2322d2fa275fb225f7d33144aae4a1cfb688
sirux98 <1000010747@studium.unict.it>
2021-11-24 12:09:58 +0100

    sixcommit

---
+++ ripasso/src/app/components/home/home.component.ts
@@ -0,0 +1,142 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  surveyform!:FormGroup;
+   str:string = JSON.stringify(this.surveyform);
+  constructor(private fb: FormBuilder) { }
+  states: string[] = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming',
+  ];
+
+ check1=false;
+ check2= false;
+ check3=false;
+
+  ngOnInit(): void {
+    this.surveyform = this.fb.group({
+      feelings: this.fb.group({
+        yes:[''],
+        no:[''],
+        maybe:['']
+      }),
+      state:[''],
+      age:['']
+    })
+  }
+
+  get feelings(){
+    return this.surveyform.get('feelings')
+  }
+
+  get state(){
+    return this.surveyform.get('state')
+  }
+
+  get age(){
+    return this.surveyform.get('age')
+  }
+
+  get value(){
+    return this.surveyform.get('value');
+  }
+  showObj(surveyform:FormGroup){
+    //alert (JSON.stringify(this.surveyform));
+   //alert(JSON.parse(this.str));
+   alert(this.surveyform.value);
+    
+  }
+ 
+  
+  onSubmit() {
+    if (this.surveyform.valid) {
+      console.log("Form Submitted!");
+     // alert(this.surveyform.value);
+   console.log(this.surveyform);
+   
+  
+  }
+}
+
+ifClicked1(){
+  this.check1=!this.check1;
+}
+ifClicked2(){
+  this.check2=!this.check2;
+  this.check1=false;
+  this.check3=false;
+}
+ifClicked3(){
+  this.check3=!this.check3;
+  this.check1=false;
+  this.check2=false;
+}
+
+/*showAlert(){
+  if(this.check1 && this.check3){
+    alert('Ecco per te uno sconto del 10%! EHFUNM');
+  }
+}*/
+
+showAlert(){
+  alert('Ecco per te uno sconto del 10%! EHFUNM');
+}
+
+}
---
+++ ripasso/src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+
+  validations_form!:FormGroup;
+  
+  adminvalue:boolean=false;
+  adminvalue1:boolean=false;
+
+  isShown: boolean = true;
+  showSpinner=false;
+
+  name: string='';
+  constructor(private fb: FormBuilder) { }
+  
+ 
+  
+  ngOnInit(): void {
+    this.validations_form = this.fb.group({
+      username:['', Validators.required
+        
+      ],
+      password:('')
+    });
+  }
+
+  funcLog(){
+    console.log(this.validations_form);
+    if(this.validations_form.get('username')?.value=== 'admin'){
+      console.log('valore esatto');
+      this.adminvalue=true;
+    }
+    if(this.validations_form.get('password')?.value ==='admin'){
+      console.log('valore esatto');
+      this.adminvalue1=true;
+    }
+  }
+
+  toggleShow() {
+
+    this.isShown = ! this.isShown;
+    
+    }
+
+    //funzione per far sparire lo spinner di loading dopo un tot tempo
+    loadData(){
+      this.showSpinner=true;
+      setTimeout(()=>{
+        this.showSpinner=false;
+      },5000);
+    }
+
+    getvalue(){
+     const res = this.validations_form.get('username');
+     return res;
+    }
+ 
+
+
+}
